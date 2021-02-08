148133cbc9f77d064a6b24eef1c6ad5474e280ea
xtaodada <xtao@xtaolink.cn>
2021-02-08 13:44:02 +0800

    init

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from pyrogram import Client
+cmd, par, des = [], [], []
 
-import logging
-
-from pyrogram import Client, idle
-
-# Enable logging
-logger = logging.getLogger(__name__)
-
-# Config session
-app = Client(
-    session_name="pagermaid"
-)
-app.start()
-
-# Hold
-idle()
-
-# Stop
-app.stop()
+if __name__ == "__main__":
+    Client("pagermaid").run()
